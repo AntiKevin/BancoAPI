cf8692040d8ac3f990d3e9fc8f855d3ed3309001
antikevin <Kevin_3467@yahoo.com>
2023-07-12 14:55:58 -0300

    FIX: definindo getters e setters da entidade

--- src/main/java/br/com/banco/entity/Conta.java
+++ src/main/java/br/com/banco/entity/Conta.java
@@ -9,4 +9,20 @@ public class Conta {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_conta;
     private String nome_responsavel;
+
+    public Long getId_conta() {
+        return id_conta;
+    }
+
+    public void setId_conta(Long id_conta) {
+        this.id_conta = id_conta;
+    }
+
+    public String getNome_responsavel() {
+        return nome_responsavel;
+    }
+
+    public void setNome_responsavel(String nome_responsavel) {
+        this.nome_responsavel = nome_responsavel;
+    }
 }
