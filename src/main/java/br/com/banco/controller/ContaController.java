7c6ecb24b9dee26ac0b920a05cbda8b7b4cb9072
antikevin <Kevin_3467@yahoo.com>
2023-07-12 16:22:56 -0300

    Feat: Criando o controller da conta

---
+++ src/main/java/br/com/banco/controller/ContaController.java
@@ -0,0 +1,36 @@
+package br.com.banco.controller;
+
+import br.com.banco.entity.Conta;
+import br.com.banco.service.ContaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/contas")
+public class ContaController {
+    private ContaService contaService;
+
+    public ContaController(ContaService contaService) {
+        this.contaService = contaService;
+    }
+
+    @PostMapping
+    List<Conta> create(@RequestBody Conta conta){
+        return contaService.create(conta);
+    }
+
+    @GetMapping
+    List<Conta> list(){
+        return contaService.list();
+    }
+    @PutMapping
+    List<Conta> update(@RequestBody Conta conta){
+        return contaService.update(conta);
+    }
+
+    @DeleteMapping("{id}")
+    List<Conta> delete(@PathVariable("id") Long id){
+        return contaService.delete(id);
+    }
+}
